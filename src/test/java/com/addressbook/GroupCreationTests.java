b233c0e6d18ec6ffe3fd7cd3ba35c37cec8d59b4
321673 <>
2019-04-03 20:36:23 +0300

    Added GroupData

--- src/test/java/com/addressbook/GroupCreationTests.java
+++ src/test/java/com/addressbook/GroupCreationTests.java
@@ -1,3 +1,4 @@
+package com.addressbook;
 import org.openqa.selenium.*;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -9,7 +10,6 @@ import static org.testng.Assert.fail;
 
 public class GroupCreationTests {
     private WebDriver driver;
-    private String baseUrl;
     private boolean acceptNextAlert = true;
     private StringBuffer verificationErrors = new StringBuffer();
     private SelenoidWebDriverProvider swdp = new SelenoidWebDriverProvider();
@@ -17,36 +17,76 @@ public class GroupCreationTests {
     @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception {
         driver = swdp.createDriver();
-        baseUrl = "https://www.katalon.com/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        logIn("admin", "secret");
     }
 
     @Test
-    public void testLesson5() throws Exception {
+    public void testGroupCreation() throws Exception {
+
+        gotoGroupPage();
+        initGroupCreation();
+        fillGroupForm(new GroupData("name", "header", "footer"));
+        submitGroupCreation();
+        returnToGroupPage();
+
+    }
+
+    public void returnToGroupPage() {
+        driver.findElement(By.linkText("group page")).click();
+    }
+
+    public void submitGroupCreation() {
+        driver.findElement(By.name("submit")).click();
+    }
+
+    public void fillGroupForm(GroupData groupData) {
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).clear();
+        driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
+        driver.findElement(By.name("group_header")).clear();
+        driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+        driver.findElement(By.name("group_footer")).clear();
+        driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    public void initGroupCreation() {
+        driver.findElement(By.name("new")).click();
+    }
+
+    public void gotoGroupPage() {
+        driver.findElement(By.linkText("groups")).click();
+    }
+
+    public void logIn(String admin, String password) {
         driver.get("http://localhost/addressbook/group.php");
         driver.findElement(By.name("user")).clear();
-        driver.findElement(By.name("user")).sendKeys("admin");
+        driver.findElement(By.name("user")).sendKeys(admin);
         driver.findElement(By.id("LoginForm")).click();
         driver.findElement(By.name("pass")).click();
         driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys("secret");
+        driver.findElement(By.name("pass")).sendKeys(password);
         driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
-        driver.findElement(By.linkText("groups")).click();
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).clear();
-        driver.findElement(By.name("group_name")).sendKeys("name");
-        driver.findElement(By.name("group_header")).clear();
-        driver.findElement(By.name("group_header")).sendKeys("header");
-        driver.findElement(By.name("group_footer")).clear();
-        driver.findElement(By.name("group_footer")).sendKeys("footer");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("group page")).click();
+    }
+
+    @Test
+    public void deleteContactTest() throws Exception {
+        driver.findElement(By.linkText("home")).click();
+        driver.findElement(By.id("MassCB")).click();
+        acceptNextAlert = true;
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Select all'])[1]/following::input[2]")).click();
+        closeAlertAndGetItsText();
+
+        //assertTrue(closeAlertAndGetItsText().matches("^Delete \\* addresses[\\s\\S]$"));
+    }
+
+    public void logOut() {
         driver.findElement(By.linkText("Logout")).click();
     }
 
     @AfterClass(alwaysRun = true)
     public void tearDown() throws Exception {
+        logOut();
         driver.quit();
         String verificationErrorString = verificationErrors.toString();
         if (!"".equals(verificationErrorString)) {
